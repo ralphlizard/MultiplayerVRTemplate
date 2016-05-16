f3283c85be8859538eec3bda7bfecc12fd709109
ralphlizard <ralph.kim93@gmail.com>
2016-05-16 16:13:54 -0400

    core features

---
+++ Assets/Scripts/VRNetworkPlayer.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class VRNetworkPlayer : NetworkBehaviour {
+
+	public Camera mainCamera;
+	public AudioListener listener;
+	public VRMouseLook vrMouseLook;
+	public GameObject reticle;
+	public enum ReticleVisible 
+	{
+		None,
+		PlayerOnly,
+		All
+	};
+	public ReticleVisible reticleVisible;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (this.gameObject);
+		if (isLocalPlayer) {
+			listener.enabled = true;
+			mainCamera.enabled = true;
+			vrMouseLook.enabled = true;
+		} 
+		else 
+		{
+			listener.enabled = false;
+			mainCamera.enabled = false;
+			vrMouseLook.enabled = false;
+		}
+
+		if (reticleVisible == ReticleVisible.None) 
+		{
+			reticle.SetActive (false);
+		} 
+		else if (reticleVisible == ReticleVisible.PlayerOnly) 
+		{
+			if (isLocalPlayer) 
+			{
+				reticle.SetActive (true);
+			} 
+			else 
+			{
+				reticle.SetActive (false);
+			}
+		} 
+		else 
+		{
+			reticle.SetActive (true);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
